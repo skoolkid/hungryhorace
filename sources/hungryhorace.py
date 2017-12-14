2e5dd6c72b0b1382655511bb8da11fcb64d3752d
Richard Dymond <rjdymond@gmail.com>
2017-12-14 18:28:13 -0400

    Implement and use the #S macro

--- sources/hungryhorace.py
+++ sources/hungryhorace.py
@@ -14,8 +14,13 @@
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
 from skoolkit.graphics import Frame, Udg
+from skoolkit.skoolasm import AsmWriter
 from skoolkit.skoolhtml import HtmlWriter
-from skoolkit.skoolmacro import parse_image_macro
+from skoolkit.skoolmacro import parse_image_macro, parse_strings
+
+def parse_s(text, index, case):
+    end, s = parse_strings(text, index, 1)
+    return end, s.lower() if case == 1 else s
 
 class HungryHoraceHtmlWriter(HtmlWriter):
     def init(self):
@@ -29,5 +34,14 @@ class HungryHoraceHtmlWriter(HtmlWriter):
         frames = [Frame(self._get_maze_udgs(addr), 2, 0, *crop_rect)]
         return end, self.handle_image(frames, fname, cwd, alt, 'ScreenshotImagePath')
 
+    def expand_s(self, text, index, cwd):
+        # #S/text/
+        return parse_s(text, index, self.case)
+
     def _get_maze_udgs(self, addr):
         return [[self.maze_tiles[i] for i in self.snapshot[a:a + 32]] for a in range(addr, addr + 768, 32)]
+
+class HungryHoraceAsmWriter(AsmWriter):
+    def expand_s(self, text, index):
+        # #S/text/
+        return parse_s(text, index, self.case)
